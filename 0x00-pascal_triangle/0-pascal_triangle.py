83684aa705fea8c8043ed50be58c92b9cdceb86b
Keny09 <rosekendy09@gmail.com>
2024-01-07 22:16:39 +0300

    Interview Questions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,19 @@
+#!/usr/bin/python3
+"""Pascal triangle module"""
+
+
 def pascal_triangle(n):
-    # Create an empty list to store the triangle
-    triangle = []
-
-    # If n <= 0, return the empty list
-    if n <= 0:
-        return triangle
-
-    # Iterate over the range of 1 to n + 1 (inclusive)
-    for i in range(1, n + 1):
-        # Append a new row to the triangle
-        triangle.append([])
-
-        # The first and last element of each row should be 1
-        triangle[i - 1].append(1)
-
-        # The remaining elements in the row should be the sum of the elements immediately above and left of the current element in the previous row
-        for j in range(1, i):
-            triangle[i - 1].append(triangle[i - 2][j - 1] + triangle[i - 2][j])
-
-        # The last element of each row should be 1
-        triangle[i - 1].append(1)
-
-    return triangle
+    """This function returns pascal triangle whose
+    size is @n
+    """
+    pascal_list = []
+    for i in range(n):
+        tmp_list = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                tmp_list.append(1)
+            else:
+                tmp_list.append(
+                        pascal_list[i - 1][j] + pascal_list[i - 1][j - 1])
+        pascal_list.append(tmp_list)
+    return pascal_list
